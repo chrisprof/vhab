e27949bdbe37e238fa46bc38c6a462b32a7dacdb
chris <94062641+chrisprof@users.noreply.github.com>
2022-06-17 14:52:53 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html', srcimg='./static/example.png')
+    return 'HELLo'
 
 if __name__ == '__main__':
     app.run()
