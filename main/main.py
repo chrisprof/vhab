00905a6b7b85d3a85b0abcc4e9c860d0082dcf09
chris <94062641+chrisprof@users.noreply.github.com>
2022-06-17 09:58:27 -0400

    made static folder

--- main/main.py
+++ main/main.py
@@ -1,12 +1,22 @@
+from tkinter import Y
 import flask
 
 from flask import Flask, render_template
 
+import matplotlib.pyplot as plt
+
 app = Flask(__name__)
+
+x=[1,2,3,4,5]
+y=[1,2,3,4,6]
+
+
+plt.plot(x,y)
+img = plt.savefig('main/static/example.png')
 
 @app.route('/')
 def main():
-    return render_template('index.html')
+    return render_template('index.html', srcimg='./static/example.png')
 
 if __name__ == '__main__':
     app.run()
