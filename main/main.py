561fdca37a6af355fc59f57a7fc2a79f8d953329
chris <94062641+chrisprof@users.noreply.github.com>
2022-06-17 09:35:28 -0400

    fixed template routing error

--- main/main.py
+++ main/main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def main():
-    return render_template('./templates/index.html')
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run()
