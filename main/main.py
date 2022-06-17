2b92f2bca3100d10c03260a99677ae492af5ab1e
chris <94062641+chrisprof@users.noreply.github.com>
2022-06-17 09:32:50 -0400

    rendering templates

--- main/main.py
+++ main/main.py
@@ -1,12 +1,12 @@
 import flask
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def main():
-    return 'Test'
+    return render_template('./templates/index.html')
 
 if __name__ == '__main__':
     app.run()
