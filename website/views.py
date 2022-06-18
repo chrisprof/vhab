2ac4f37fcacaf8589a30aab22af9f902e706e96a
aryamank12345 <aryamankapoor15@gmail.com>
2022-06-18 13:00:56 -0400

    drug database file

--- website/views.py
+++ website/views.py
@@ -3,17 +3,22 @@ from flask import Blueprint, render_template
 views = Blueprint('views', __name__)
 
 
-
 @views.route('/')
 @views.route('/home')
 def home():
     return render_template("home.html")
 
+
 @views.route('/profile')
 def profile():
     return render_template("profile.html")
+
 
 @views.route('/resources')
 def resources():
     return render_template("resources.html")
 
+
+@views.route('/drug')
+def drug():
+    return render_template("drug.html")
