460439a42d42f112ee86f747071a1e7068862b2b
chris <94062641+chrisprof@users.noreply.github.com>
2022-06-18 18:03:15 -0400

    changes

--- website/static/js/profile.js
+++ website/static/js/profile.js
@@ -1,13 +1,50 @@
 let dayCount = document.getElementById('clean-days')
-let drug = document.get
+let drug = document.getElementById('drug')
+let todaysdose = document.getElementById('todays-dose')
+let name = document.getElementById('name')
+let htmlpercentagedecrease= document.getElementById('percentage-loss')
+
 
 function updateRender()
 {
     if(localStorage.getItem('days'))
     {
         dayCount.innerHTML=localStorage.getItem('days')
+        drug.innerHTML = localStorage.getItem('drug')
+
     }
+
     else{
         window.location.href='/'
     }
 }
+
+let graph = document.getElementById('dosageGraph')
+
+localStorage.setItem('days',1)
+
+localStorage.setItem('doses','[2]')
+
+let days = []
+let doses = JSON.parse(localStorage.getItem('doses'))
+let percentageDecrease = +Math.floor(doses[doses.length-1]/doses[0]*100)+'%'
+
+htmlpercentagedecrease.innerHTML=percentageDecrease
+
+for(var i = 0; i<Number(localStorage.getItem('days')); i++)
+{
+    days.push(i+1)
+}
+
+let myGraph = new Chart(graph, {
+    type:'line',
+    data: {
+        labels: days,
+        datasets: [{
+            label:'dosage',
+            backgroundColor: '#5F7A61',
+            pointBorderWidth: 15,
+            data: doses
+        }]
+    }
+})
--- website/views.py
+++ website/views.py
@@ -3,6 +3,7 @@ from flask import Blueprint, render_template
 views = Blueprint('views', __name__)
 
 
+
 @views.route('/')
 @views.route('/home')
 def home():
