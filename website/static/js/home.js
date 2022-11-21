3e13384db869112599539c1212052e19827886cf
chris <94062641+chrisprof@users.noreply.github.com>
2022-11-20 20:50:59 -0500

    fixed console log

--- website/static/js/home.js
+++ website/static/js/home.js
@@ -1,7 +1,5 @@
 let lastPosYsc = 0;
 
-console.log("hello");
-
 function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
@@ -10,12 +8,11 @@ function getRandomInt(min, max) {
 
 document.addEventListener("scroll", function (e) {
   lastPosYsc = Math.floor(window.scrollY / 30.946);
-  let multiplier = 1;
+  let multiplier = 1.25;
   let r = 213 - lastPosYsc * multiplier;
   let g = 238 - lastPosYsc * multiplier;
   let b = 187 - lastPosYsc * multiplier;
   document.body.style.backgroundColor = "rgb(" + r + "," + g + "," + b + ")";
-  console.log(lastPosYsc);
 });
 
 document.addEventListener('mousemove',function(){
